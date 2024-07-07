5fff55aa724529a2f9ce9273f5d5e16d0fd47366
BL-CZY <jiguangllsw@gmail.com>
2024-07-07 10:52:35 -0700

    nav

--- svelte/src/lib/nav.svelte
+++ svelte/src/lib/nav.svelte
@@ -1,13 +1,13 @@
 <style>
     #topnav {
-        background-color: white;
-        border-bottom: 1px solid black;
+        background-color: rgb(240, 240, 240);;
+        border-bottom: 1px solid rgb(105, 105, 105);
         padding: 2px 70px 2px 70px;
         overflow: hidden;
     }
 
     #left {
-        flex: 1;
+        flex: 6;
         display: flex;
     }
 
@@ -19,19 +19,80 @@
     
     #search {
         display: inline;
-        flex: 4;
+        flex: 5;
     }
 
     #icon {
         flex: 1;
+        padding-top: 6px;
     }
 
-    #about {
-        flex: 5;
-    }
-
+    #about,
     #source {
         flex: 1;
+        padding-top: 6px;
+        position: relative;
+    }
+
+    #input-wrapper {
+        display: inline;
+        position: relative;
+    }
+
+    #search-btn {
+        position: absolute;
+        top: -8%;
+        right: calc(0% + 8px);
+    }
+
+    #special-char-btn1 {
+        position: absolute;
+        top: -8%;
+        right: calc(0% + 62px);
+    }
+
+    #special-char-btn2 {
+        position: absolute;
+        top: -8%;
+        right: calc(0% + 81px);
+    }
+
+    #special-char-btn3 {
+        position: absolute;
+        top: -8%;
+        right: calc(0% + 102px);
+    }
+
+    #special-char-btn4 {
+        position: absolute;
+        top: -8%;
+        right: calc(0% + 123px);
+    }
+
+    #search-bar {
+        width: 70%;
+        transition: all, 0.5s;
+        border-radius: 5px;
+        border: 1px solid rgb(105, 105, 105);
+        padding: 5px;
+    }
+
+    #search-bar:focus {
+        outline: none;
+        border-color: black;
+    }
+
+    button {
+        border-radius: 5px;
+        transition: all 0.5s;
+        border: 1px solid rgb(105, 105, 105);
+        background-color: rgb(240, 240, 240);
+        color: rgb(105, 105, 105);
+    }
+
+    button:hover {
+        background-color: white;
+        color: black;
     }
     
     ul {
@@ -45,9 +106,8 @@
     }
 
     a {
-        color: black;
+        color: rgb(105, 105, 105);
         transition: all 0.5s;
-        text-decoration: underline;
     }
 
     a:link,
@@ -55,6 +115,77 @@
     a:visited,
     a:active {
         text-decoration: none;
+    }
+
+    a:hover {
+        color: black;
+    }
+
+    a:hover::after {
+        position: absolute;
+        content: '';
+        display: block;
+        height: 1.5px;
+        background-color: red;
+        bottom: 1px;
+    }
+
+    a::after {
+        position: absolute;
+        content: '';
+        display: block;
+        height: 1.5px;
+        background-color: red;
+        bottom: 1px;
+        transition: all, 0.5s;
+    }
+
+    #about-link::after {
+        right: 26px;
+        width: 0;
+    }
+
+    #about-link:hover::after {
+        right: -2px;
+        width: 50px;
+    }
+
+    #source-link::after {
+        right: 26px;
+        width: 0;
+    }
+
+    #source-link:hover::after {
+        right: -2px;
+        width: 58px;
+    }
+
+    @media only screen and (max-width: 550px) {
+        .special-char-btn {
+            display: none !important;
+        }
+
+        #search-bar {
+            width: 40%;
+        }
+
+        #search-bar:active {
+            width: 40%;
+        }
+    }
+
+    @media only screen and (max-width: 460px) {
+        #search-btn {
+            display: none !important;
+        }
+
+        #search-bar {
+            width: 100%;
+        }
+
+        #search-bar:active {
+            width: 100%;
+        }
     }
 </style>
 
@@ -65,22 +196,24 @@
                 <a href="/">dictionary</a>
             </li>
             <li id="search">
-                <div>
-                    <button on:click={() => appendSpecialLetter("ċ")}>ċ</button>
-                    <button on:click={() => appendSpecialLetter("ġ")}>ġ</button>
-                    <button on:click={() => appendSpecialLetter("ħ")}>ħ</button>
-                    <button on:click={() => appendSpecialLetter("ż")}>ż</button>
-                    <input id="search-bar" bind:value={inputVal} />
-                    <button>search</button>
+                <div id="search-wrapper">
+                    <div id="input-wrapper">
+                        <button id="special-char-btn1" class="special-char-btn" on:click={() => appendSpecialLetter("ċ")}>ċ</button>
+                        <button id="special-char-btn2" class="special-char-btn" on:click={() => appendSpecialLetter("ġ")}>ġ</button>
+                        <button id="special-char-btn3" class="special-char-btn" on:click={() => appendSpecialLetter("ħ")}>ħ</button>
+                        <button id="special-char-btn4" class="special-char-btn" on:click={() => appendSpecialLetter("ż")}>ż</button>
+                        <input id="search-bar" placeholder="  search..." bind:value={inputVal}>
+                        <button id="search-btn">search</button>
+                    </div>
                 </div>
             </li>
         </div>
         <div id="right">
             <li id="about">
-                <a href="/about">about</a>
+                <a href="/about" id="about-link">about</a>
             </li>
             <li id="source">
-                <a href="https://github.com/BL-CZY/malti">source</a>
+                <a href="https://github.com/BL-CZY/malti" id="source-link">source</a>
             </li>
         </div>
     </ul>
