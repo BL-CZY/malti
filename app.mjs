b036e86adff913587f64c9482bc633e55e39e0a1
BL-CZY <jiguangllsw@gmail.com>
2024-07-18 22:04:27 -0700

    now to frontend

--- app.mjs
+++ app.mjs
@@ -1,63 +1,56 @@
 "use strict";
 
-import express from 'express'
-import { handler } from './svelte/build/handler.js'
-import { search } from '@jg-tpl/malti_search';
+import express from "express";
+import { handler } from "./svelte/build/handler.js";
+import { getWord, search } from "@jg-tpl/malti_search";
 
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
     console.log(`app running on port ${port}`);
 });
 
-app.get('/api/search', (req, res) => {
+app.get("/api/search", (req, res) => {
     let keyword = req.query.keyword;
-    let exact = Boolean(req.query.exact);
-    let text = Boolean(req.query.text);
-    let skip = req.query.skip;
-    let limit = req.query.limit;
+    let skip = parseInt(req.query.skip);
+    let limit = parseInt(req.query.limit);
+    let maxDis = parseInt(req.query.maxDis);
 
     if (!keyword) {
         res.status(404).write("invalid keyword");
         return;
     }
 
-    try {
-        skip = Math.max(parseInt(skip), 0)
-    } catch (_) {
-        console.log(e);
+    if (isNaN(skip)) {
         skip = 0;
     }
 
-    try {
-        limit = Math.max(parseInt(limit), 0);
-    } catch (_) {
-        console.log(e);
+    if (isNaN(limit)) {
         limit = 10;
     }
 
-    let searched = search(keyword, exact, text, skip, limit);
-    let result = {
-        words: []
-    };
+    if (isNaN(maxDis)) {
+        maxDis = 3;
+    } 
 
-    searched.forEach((str) => {
-        if(str.startsWith("\"")) {
-            str = str.substring(1);
-        }
-
-        if(str.endsWith("\"")) {
-            str = str.substring(0, str.length - 1);
-        }
-        result.words.push(str);
-    });
-
-    res.write(JSON.stringify(result));
+    res.write(search(keyword, skip, limit, maxDis));
 
     res.end();
 });
+
+app.get("/api/fetch", (req, res) => {
+    let keyword = req.query.keyword;
+    if (!keyword) {
+        res.status(404).write("invalid keyword");
+        return;
+    }
+
+    res.write(getWord(keyword));
+    
+    res.end();
+})
 
 app.use(handler);
